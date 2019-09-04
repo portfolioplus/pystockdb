a23f4ee5d2d85e6fb3bff7006a489b427d9048fe
Slash <slash.gordon.dev@gmail.com>
2019-09-04 17:55:03 +0200

    Bump pytickersymbols from 1.1.7 to 1.1.8.

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@
 from setuptools import setup, find_packages
 
 EXCLUDE_FROM_PACKAGES = ['test', 'test.*', 'test*']
-VERSION = '1.0.10'
+VERSION = '1.0.11'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 INSTALL_REQUIRES = (
     [
-        'pytickersymbols>=1.1.7',
+        'pytickersymbols>=1.1.8',
         'pandas==0.25.1',
         'yfinance>=0.1.44',
         'uplink>=0.9.0',
